463f307a7d1b4394694733d61af0a2133f21c0fc
Mahshid Azami <m.azami2008@gmail.com>
2018-11-27 21:13:37 +0000

    week-5 dom & callback done

--- week-5/A-dom-manipulation/exercise.js
+++ week-5/A-dom-manipulation/exercise.js
@@ -4,8 +4,11 @@ Task 1
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
-
+const x = document.getElementById("bgrChangeBtn");
+x.addEventListener("click", changeColour);
+function changeColour() {
+  document.body.style.backgroundColor = "blue";
+}
 
 /*
 Task 2
@@ -13,8 +16,11 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
-
+function popUp() {
+  alert("Thanks for visiting Bikes for Refugees!");
+}
+const y = document.getElementById("alertBtn");
+y.addEventListener("click", popUp);
 
 /*
 Task 3
@@ -23,11 +29,22 @@ Task 3
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
 
-
-
+const someText = document.getElementById("addTextBtn");
+someText.addEventListener("click", addSomeText);
+function addSomeText() {
+  const newPar = document.createElement("p");
+  const button = document.querySelector(".buttons");
+  button.appendChild(newPar);
+  newPar.innerText = "Read more below.";
+}
 /*
 Task 4
 ======
 
-When the 'Larger links!' button is clicked, the text of all links on the page should increase.
-*/
+When the 'Larger links!' button is clicked, the text of all links on the page should increase.*/
+const largerBotton = document.getElementById("largerLinksBtn");
+largerBotton.addEventListener("click", largerLink);
+function largerLink() {
+  const links = document.querySelectorAll("a");
+  links.forEach(item => (item.style.fontSize = "2em"));
+}
--- week-5/B-callbacks/exercise.js
+++ week-5/B-callbacks/exercise.js
@@ -14,9 +14,10 @@
   EXPECTED RESULT: The #exercise1 element has textContent = "YOUR NAME" when
   the button is clicked
 */
-document.querySelector('#button1').addEventListener('click', exercise1)
+document.querySelector("#button1").addEventListener("click", exercise1);
 
 function exercise1() {
+  document.getElementsByTagName("h3")[0].innerText = "Mahshid Azami";
   // Write your implementation here
 }
 
@@ -34,9 +35,10 @@ function exercise1() {
   EXPECTED RESULT: The #exercise2 element has textContent = "Hello from the
   function caller"
 */
-functionThatCallsBack(exercise2)
+functionThatCallsBack(exercise2);
 
 function exercise2(result) {
+  document.getElementById("exercise2").innerText = result;
   // Write your implementation here
 }
 
@@ -57,23 +59,23 @@ function exercise2(result) {
 */
 
 function exercise3(callback) {
+  callback("Hello from the function caller");
   // Write your implementation here
-
   // Write your explanation here
 }
 
-// 
+//
 // -------------------------------------
-// 
+//
 // DON'T EDIT THE FUNCTIONS BELOW!
 // THEY ARE PROVIDED FOR YOU
 //
 // -------------------------------------
 //
 function functionThatCallsBack(callback) {
-  callback('Hello from the function caller')
+  callback("Hello from the function caller");
 }
 
-exercise3((text) => {
-  document.querySelector('#exercise3').innerText = text
-})
+exercise3(text => {
+  document.querySelector("#exercise3").innerText = text;
+});
